dd02fe3f05d8a239678e8c4f3bd1c2d187ea0b94
Resham Tamang <moktanresham3@gmail.com>
2026-03-15 20:32:52 +0545

    post ko api

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { books } = require("./database/connection");
 const app = express()
 
 require("./database/connection");
-
+app.use(express.json())
 
 app.get("/books",async function(req,res){
     // logic code to fetch goes here
@@ -15,9 +15,21 @@ app.get("/books",async function(req,res){
         datas  // js ma key ra value same vaye-- datas matra lekhda hunxa
     })
 })
-app.post("/books",function(req,res){
+
+app.post("/books",async function(req,res){
     // logic code goes here
     
+console.log(req.body)
+// const bookName = req.body.bookName
+// const bookPrice = req.body.bookPrice
+const {bookName,bookPrice, bookAuthor, bookGenre} = req.body;
+// check if all the dataaako xa vane proceed, esle not proceed throw error
+await books.create({
+    bookName,
+    bookPrice,
+    bookAuthor,
+    bookGenre
+});
 
     res.json({
         messsage: "Book Added SuccessFully"
