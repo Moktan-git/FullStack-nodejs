dc3d9da11d0566e2491692f5af76c09b587481ae
Resham Tamang <moktanresham3@gmail.com>
2026-03-15 18:17:15 +0545

    getbookslogicode

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require("express")
+const express = require("express");
+const { books } = require("./database/connection");
 const app = express()
 
 require("./database/connection");
 
 
-app.get("/books",function(req,res){
+app.get("/books",async function(req,res){
+    // logic code to fetch goes here
+
+    const datas = await books.findAll() //slect * from books--querry ma books.find()--> mongoose ma 
+
     res.json({
-        messsage: "Book Fetched SuccessFully"
+        messsage: "Book Fetched SuccessFully",
+        datas  // js ma key ra value same vaye-- datas matra lekhda hunxa
     })
 })
 app.post("/books",function(req,res){
+    // logic code goes here
+    
+
     res.json({
         messsage: "Book Added SuccessFully"
     })
 })
 app.patch("/books/:id",function(req,res){
+    // logic code goes here
+
     res.json({
         messsage: "Book Updated SuccessFully"
     })
 })
 app.delete("/books/:id",function(req,res){
+    // logic code goes here
+
     res.json({
         messsage: "Book Deleted SuccessFully"
     })
